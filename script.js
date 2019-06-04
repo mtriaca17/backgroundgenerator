0e671c7df90bcc2f3acf13618287bd1b2ee6ce1c
Miguel <migueltriaca@gmail.com>
2019-06-04 18:46:27 +0800

    text input update changes color with random button

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var cssText = document.querySelector("h3");
+var cssText = document.querySelector(".gradientStringRGB");
+var hexText = document.querySelector(".gradientStringHex")
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.querySelector("body");
@@ -36,7 +37,33 @@ function setGradient(){
 }
 
 function setRandomGradient(){
-    body.style.background = "linear-gradient(to right, " + createRGB() + "," + createRGB() +")"
-    cssText.textContent = body.style.background + ";";
+    rgb1 = createRGB()
+    rgb2 = createRGB()
     
+    body.style.background = "linear-gradient(to right, " + rgb1 + "," + rgb2 +")"
+    color1.value = stringRGB(rgb1) 
+    color2.value = stringRGB(rgb2) 
+    cssText.textContent = body.style.background + ";";
+    hexText.textContent = "Hex Code 1: "+ color1.value +  " Hex Code 2: " + color2.value;
+}
+
+function RGBToHex(r,g,b){
+    return "#" + toHex(r)+ toHex(g) + toHex(b)
+    // console.log("#"+toHex(r)+ toHex(g)+ toHex(b))
+
+}
+
+function toHex(n){
+    n = parseInt(n, 10);
+    if(isNaN(n)) return "00";
+    n = Math.max(0, Math.min(n,255));
+    return "0123456789ABCDEF".charAt((n-n%16)/16)
+            + "0123456789ABCDEF".charAt(n%16)    
+}
+
+
+function stringRGB(rgb){
+    rgbString = rgb.slice(4, -1)
+    rgbSplitString = rgbString.split(',')
+    return RGBToHex(rgbSplitString[0], rgbSplitString[1], rgbSplitString[2])
 }
